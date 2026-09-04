546340096a3d638be504f006fc915d841feac5a6
unknown <you@example.com>
2026-09-04 22:20:11 +0300

    feat: redesign landing and configure Firebase

--- site-config.js
+++ site-config.js
@@ -1,7 +1,10 @@
 // Firebase web configuration is public by design. Access is protected by firestore.rules.
 const FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: "",
+  apiKey: "AIzaSyBTdcqFwic1VCH5cR55njV4Dshqije73_o",
+  authDomain: "worcline-project.firebaseapp.com",
+  projectId: "worcline-project",
+  storageBucket: "worcline-project.firebasestorage.app",
+  messagingSenderId: "342068198500",
+  appId: "1:342068198500:web:9d0a8e7fd6d1087043b371",
+  measurementId: "G-QMVFM6GY72",
 };
